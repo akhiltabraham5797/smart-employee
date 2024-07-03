fb95c77d60d9c64e2b8385f233c450ab6018e349
kanvi-creator <84613343+kanvi-creator@users.noreply.github.com>
2024-07-03 16:21:46 -0400

    Project manager dashboard with project manager profile and tasks added

--- projectmanager.php
+++ projectmanager.php
@@ -36,3 +36,66 @@ if(isset($_SESSION['user_id'])) {
     </div>
   </nav>
 </header>
+
+
+<main>
+  <h1 class="emp_head">Project Manager Dashboard</h1>
+  <div class="emp_box">
+
+    <div class="emp_box_left">
+      <div class="prof_img">
+        <img src="assets/images/profile-icon.png">
+      </div>
+      <div class="emp_profile_box">
+        <div class="emp_profile_cont">
+          <label>Name:</label>
+          <p><?php echo $prof_details['first_name']; ?> <?php echo $prof_details['last_name']; ?></p>
+        </div>
+        <div class="emp_profile_cont">
+          <label>Address:</label>
+          <p><?php echo $prof_details['address']; ?></p>
+        </div>
+        <div class="emp_profile_cont">
+          <label>Email:</label>
+          <p><?php echo $prof_details['email']; ?></p>
+        </div>
+        <div class="emp_profile_cont">
+          <label>Designation:</label>
+          <p><?php echo $prof_details['job_title']; ?></p>
+        </div>
+      </div>
+      
+    </div>
+    <?php
+    $task_data = $pdo->prepare("SELECT * FROM tasks WHERE assigned_to=$emp_id");
+    $task_data->execute();?>
+    <div class="emp_box_center">
+      <h3>Projects & Tasks</h3>
+      <div class="emp_tasks">
+      <?php while ($task_details = $task_data->fetch(PDO::FETCH_ASSOC)) {
+        ?>
+        <a href="Task_Details.php?id='<?php echo $task_details['task_id'];?>'" class="emp_task_box">
+          <div>
+            <h4><?php echo $task_details['task_name']; ?></h4>
+            <p class="mb-0"><?php echo $task_details['end_date']; ?></p>
+          </div>
+          <div class="more_details">
+              <span class="mr-2">More Details</span>
+              <div style="width:20px;">
+                <img src="assets/images/right-arrows.png" class="w-100 d-block">
+              </div>
+          </div>
+        </a>
+      <?php
+      }
+      ?>
+      </div>
+    </div>
+    </div>
+  </div>
+</main>
+</body>
+<?php                
+    }
+    ?>
+</html>
