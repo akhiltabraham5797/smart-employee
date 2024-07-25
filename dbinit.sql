9c95fcaa8fbc9a2a5f46b2dd9698690c0040ea65
noobie1028 <ajoshy7874@conestogac.on.ca>
2024-07-25 01:35:18 -0400

    changes in the db for leave request and salary generation

--- dbinit.sql
+++ dbinit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2024 at 04:58 AM
+-- Generation Time: Jul 25, 2024 at 07:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,7 +46,19 @@ INSERT INTO `attendance` (`attendance_id`, `user_id`, `date`, `work_hours`, `clo
 (3, 2, '2023-06-01', 0, '08:30:00', '17:30:00'),
 (4, 17, '2024-07-25', 0, '01:42:21', '01:56:14'),
 (5, 17, '2024-07-25', 0, '01:58:06', '02:15:06'),
-(6, 17, '2024-07-25', 0, '02:15:18', NULL);
+(6, 17, '2024-07-25', 0, '02:15:18', NULL),
+(7, 15, '2024-07-25', 0, '06:04:48', '06:12:11'),
+(8, 14, '2024-07-25', 0, '06:06:00', '06:12:42'),
+(9, 16, '2024-07-25', 0, '06:06:10', NULL),
+(10, 15, '2024-07-25', 0, '06:07:53', '06:12:11'),
+(11, 15, '2024-07-25', 0, '06:11:38', '06:12:11'),
+(12, 14, '2024-07-25', 0, '06:12:29', '06:12:42'),
+(13, 16, '2024-07-25', 0, '06:12:53', NULL),
+(14, 14, '2024-07-25', 0, '06:31:39', NULL),
+(15, 16, '2024-07-25', 0, '06:32:13', NULL),
+(16, 15, '2024-07-25', 0, '07:14:52', '14:17:27'),
+(17, 15, '2024-07-25', 0, '07:18:13', NULL),
+(18, 16, '2024-07-25', 0, '07:26:40', NULL);
 
 -- --------------------------------------------------------
 
@@ -105,7 +117,8 @@ INSERT INTO `leaverequests` (`leave_id`, `user_id`, `LeaveType`, `start_date`, `
 (7, 1, 'sick', '2024-07-30', '2024-08-02', 'Dental surgery', 'Rejected', '2024-07-25', 'rejected', NULL, NULL),
 (8, 2, 'casual', '2024-08-10', '2024-08-15', 'Short vacation', 'Rejected', '2024-07-30', 'Weekend getaway.', 12, '2024-07-23'),
 (9, 3, 'sick', '2024-09-01', '2024-09-03', 'Migraine', 'Rejected', '2024-08-28', 'Severe migraine attacks.', 12, '2024-07-23'),
-(10, 1, 'earned', '2024-11-20', '2024-11-30', 'Long vacation', 'Approved by HR', '2024-10-15', 'Extended holiday break.', 12, '2024-07-23');
+(10, 1, 'earned', '2024-11-20', '2024-11-30', 'Long vacation', 'Approved by HR', '2024-10-15', 'Extended holiday break.', 12, '2024-07-23'),
+(11, 15, 'casual', '2024-07-24', '2024-07-26', 'something', 'Rejected', '2024-07-25', 'yess', 16, '2024-07-25');
 
 -- --------------------------------------------------------
 
@@ -180,7 +193,10 @@ INSERT INTO `salaries` (`salary_id`, `user_id`, `amount`, `payment_date`, `pay_p
 (16, 14, 2900.00, '2024-07-31', '2024-07-01', '2024-07-31', 'Project N', 36.25, 80.00),
 (17, 15, 3200.00, '2024-07-31', '2024-07-01', '2024-07-31', 'Project O', 40.00, 80.00),
 (18, 16, 2700.00, '2024-07-31', '2024-07-01', '2024-07-31', 'Project P', 33.75, 80.00),
-(19, 17, 3000.00, '2024-07-31', '2024-07-01', '2024-07-31', 'Project Q', 37.50, 80.00);
+(19, 17, 3000.00, '2024-07-31', '2024-07-01', '2024-07-31', 'Project Q', 37.50, 80.00),
+(20, 15, 144.33, '2024-07-25', '2024-07-01', '2024-07-31', NULL, 20.00, 7.22),
+(21, 15, 146.80, '2024-07-25', '2024-07-09', '2024-07-31', NULL, 20.00, 7.34),
+(22, 15, 147.00, '2024-07-25', '2024-07-09', '2024-07-31', NULL, 20.00, 7.35);
 
 -- --------------------------------------------------------
 
@@ -232,31 +248,32 @@ CREATE TABLE `users` (
   `emergency_contact_name` varchar(100) DEFAULT NULL,
   `emergency_contact_phone` varchar(15) DEFAULT NULL,
   `emergency_contact_relation` varchar(100) DEFAULT NULL,
-  `project_id` int(11) DEFAULT NULL
+  `project_id` int(11) DEFAULT NULL,
+  `hourly_rate` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `first_name`, `last_name`, `email`, `phone`, `address`, `date_joined`, `department`, `job_title`, `date_of_birth`, `emergency_contact_name`, `emergency_contact_phone`, `emergency_contact_relation`, `project_id`) VALUES
-(1, 'jdoe', 'password123', 'employee', 'John', 'Doe', 'john.doe@example.com', '555-1234', '123 Elm Street', '2020-01-15', 'Development', 'Developer', '1990-05-15', 'Jane Doe', '555-5678', 'Spouse', 3),
-(2, 'asmith', 'password456', 'project manager', 'Alice', 'Smith', 'alice.smith@example.com', '555-5678', '456 Oak Street', '2019-03-12', 'Development', 'Project Manager', '1985-09-20', 'Bob Smith', '555-8765', 'Husband', 2),
-(3, 'mbrown', 'password789', 'HR', 'Mary', 'Brown', 'mary.brown@example.com', '555-8765', '789 Pine Street', '2021-06-22', 'Human Resources', 'HR Manager', '1988-11-30', 'Tom Brown', '555-3456', 'Brother', 1),
-(4, 'ann45', '77wP6sL5+s', 'Manager', 'Sandra', 'Watson', 'tarasanders@guerra-norris.com', '387.753.2334', '6822 William Turnpike Suite 100\nWest Debra, MT 01367', '2023-10-14', 'IT', 'Coordinator', '1990-09-07', 'Philip Cox', '(087)086-2483', 'Spouse', 3),
-(5, 'sanchezchristopher', '^&2NhhSy*c', 'Manager', 'Penny', 'Huang', 'alexisbeasley@yahoo.com', '+1-343-681-7053', '0045 Oscar Inlet Apt. 634\nEast Todd, TN 76406', '2023-08-01', 'IT', 'Coordinator', '1991-04-05', 'Catherine Lawrence', '338.382.8214x36', 'Parent', 3),
-(6, 'jamesbrown', 'wz6V&SAh1$', 'Employee', 'Tracy', 'Cortez', 'johnsonmonica@simpson.com', '390-579-7472x06', '4268 Brian Bypass Suite 489\nPort Stephen, LA 12358', '2021-12-27', 'IT', 'Consultant', '1992-11-24', 'Regina Cox', '704.172.3227x81', 'Parent', 3),
-(7, 'katelyn26', '@V(6k5NnIV', 'Manager', 'Alice', 'Blanchard', 'timothy82@hotmail.com', '(536)638-8550', '68378 Jones Falls Suite 746\nDeborahside, MT 38539-3078', '2020-08-03', 'HR', 'Manager', '1997-06-19', 'Jessica Lee', '7412843022', 'Sibling', 4),
-(8, 'thomas48', '*0yMda6Zy9', 'Employee', 'Douglas', 'Adams', 'sarah08@clark-nguyen.com', '+1-797-989-0162', '005 Martinez Pike\nPort Allisonborough, MT 95646', '2021-08-13', 'Sales', 'Analyst', '1989-04-03', 'Nathan Dunn', '001-369-417-100', 'Sibling', 5),
-(9, 'michaeljordan', 'zB2%6ht8L$', 'Employee', 'Michael', 'Jordan', 'michael.jordan@example.com', '123-456-7890', '123 Main St\nSpringfield, IL 62704', '2022-01-15', 'Marketing', 'Specialist', '1985-06-14', 'Sarah Jordan', '234-567-8901', 'Spouse', 3),
-(10, 'emilyclark', 'P4ssW0rd!7$', 'Manager', 'Emily', 'Clark', 'emily.clark@example.com', '789-012-3456', '456 Elm St\nBrooklyn, NY 11201', '2023-03-05', 'Finance', 'Director', '1990-12-10', 'John Clark', '890-123-4567', 'Parent', 4),
-(11, 'johnsmith', 'Qw3rtY&78', 'Employee', 'John', 'Smith', 'john.smith@example.com', '456-789-0123', '789 Pine St\nSeattle, WA 98101', '2021-06-20', 'Engineering', 'Engineer', '1987-08-22', 'Jane Smith', '567-890-1234', 'Sibling', 4),
-(12, 'laurawilson', 'H3ll0W0rld$', 'Employee', 'Laura', 'Wilson', 'laura.wilson@example.com', '234-567-8901', '987 Oak St\nSan Francisco, CA 94102', '2022-07-30', 'HR', 'Recruiter', '1993-04-12', 'Paul Wilson', '345-678-9012', 'Parent', 8),
-(13, 'davidlee', 'R3d@ppl3#8', 'Employee', 'David', 'Lee', 'david.lee@example.com', '678-901-2345', '321 Birch St\nBoston, MA 02118', '2021-04-11', 'Operations', 'Coordinator', '1991-11-05', 'Linda Lee', '789-012-3456', 'Spouse', 10),
-(14, 'userone@mail.com', '$2y$10$TyVjOXMSbiBXTqzup54Yd.GkQG/XtVg82X6xOnSo.0BHRMhjqjJq6', 'project manager', 'user', 'one', 'userone@mail.com', '1111111111', 'address 5', '2024-07-01', 'Front end', 'project manager', '2024-07-02', NULL, NULL, NULL, NULL),
-(15, 'empone@mail.com', '$2y$10$jg9JANbRXR8yypI2nakXGeMnFkUCRsTmCpZcuPJO4IuAGlmsKCE/2', 'employee', 'emp', 'one', 'empone@mail.com', '1111111111', 'address 2', '2024-07-01', 'back end', 'developer', '2024-07-01', NULL, NULL, NULL, 4),
-(16, 'ob@mail.com', '$2y$10$WEiofwoUwxCzyc6KuX4r6uaXK/g00iaCuTL0J8Vy9UTC58H5.KGMG', 'HR', 'Bob', 'Brown', 'ob@mail.com', '1111111111', 'address123', '2020-08-01', 'HR', 'HR', '1996-01-01', NULL, NULL, NULL, NULL),
-(17, 'anaij@mail.com', '$2y$10$xR8muB7UXejd.9X0jPU6neEKoifzeOrv8BYno8tKG70Js9SAN7Mti', 'employee', 'anai', 'joshy', 'anaij@mail.com', '1234567890', 'address231', '2024-07-01', 'development', 'employee', '2000-02-08', NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `first_name`, `last_name`, `email`, `phone`, `address`, `date_joined`, `department`, `job_title`, `date_of_birth`, `emergency_contact_name`, `emergency_contact_phone`, `emergency_contact_relation`, `project_id`, `hourly_rate`) VALUES
+(1, 'jdoe', 'password123', 'employee', 'John', 'Doe', 'john.doe@example.com', '555-1234', '123 Elm Street', '2020-01-15', 'Development', 'Developer', '1990-05-15', 'Jane Doe', '555-5678', 'Spouse', 3, 0),
+(2, 'asmith', 'password456', 'project manager', 'Alice', 'Smith', 'alice.smith@example.com', '555-5678', '456 Oak Street', '2019-03-12', 'Development', 'Project Manager', '1985-09-20', 'Bob Smith', '555-8765', 'Husband', 2, 0),
+(3, 'mbrown', 'password789', 'HR', 'Mary', 'Brown', 'mary.brown@example.com', '555-8765', '789 Pine Street', '2021-06-22', 'Human Resources', 'HR Manager', '1988-11-30', 'Tom Brown', '555-3456', 'Brother', 1, 0),
+(4, 'ann45', '77wP6sL5+s', 'Manager', 'Sandra', 'Watson', 'tarasanders@guerra-norris.com', '387.753.2334', '6822 William Turnpike Suite 100\nWest Debra, MT 01367', '2023-10-14', 'IT', 'Coordinator', '1990-09-07', 'Philip Cox', '(087)086-2483', 'Spouse', 3, 0),
+(5, 'sanchezchristopher', '^&2NhhSy*c', 'Manager', 'Penny', 'Huang', 'alexisbeasley@yahoo.com', '+1-343-681-7053', '0045 Oscar Inlet Apt. 634\nEast Todd, TN 76406', '2023-08-01', 'IT', 'Coordinator', '1991-04-05', 'Catherine Lawrence', '338.382.8214x36', 'Parent', 3, 0),
+(6, 'jamesbrown', 'wz6V&SAh1$', 'Employee', 'Tracy', 'Cortez', 'johnsonmonica@simpson.com', '390-579-7472x06', '4268 Brian Bypass Suite 489\nPort Stephen, LA 12358', '2021-12-27', 'IT', 'Consultant', '1992-11-24', 'Regina Cox', '704.172.3227x81', 'Parent', 3, 0),
+(7, 'katelyn26', '@V(6k5NnIV', 'Manager', 'Alice', 'Blanchard', 'timothy82@hotmail.com', '(536)638-8550', '68378 Jones Falls Suite 746\nDeborahside, MT 38539-3078', '2020-08-03', 'HR', 'Manager', '1997-06-19', 'Jessica Lee', '7412843022', 'Sibling', 4, 0),
+(8, 'thomas48', '*0yMda6Zy9', 'Employee', 'Douglas', 'Adams', 'sarah08@clark-nguyen.com', '+1-797-989-0162', '005 Martinez Pike\nPort Allisonborough, MT 95646', '2021-08-13', 'Sales', 'Analyst', '1989-04-03', 'Nathan Dunn', '001-369-417-100', 'Sibling', 5, 0),
+(9, 'michaeljordan', 'zB2%6ht8L$', 'Employee', 'Michael', 'Jordan', 'michael.jordan@example.com', '123-456-7890', '123 Main St\nSpringfield, IL 62704', '2022-01-15', 'Marketing', 'Specialist', '1985-06-14', 'Sarah Jordan', '234-567-8901', 'Spouse', 3, 0),
+(10, 'emilyclark', 'P4ssW0rd!7$', 'Manager', 'Emily', 'Clark', 'emily.clark@example.com', '789-012-3456', '456 Elm St\nBrooklyn, NY 11201', '2023-03-05', 'Finance', 'Director', '1990-12-10', 'John Clark', '890-123-4567', 'Parent', 4, 0),
+(11, 'johnsmith', 'Qw3rtY&78', 'Employee', 'John', 'Smith', 'john.smith@example.com', '456-789-0123', '789 Pine St\nSeattle, WA 98101', '2021-06-20', 'Engineering', 'Engineer', '1987-08-22', 'Jane Smith', '567-890-1234', 'Sibling', 4, 0),
+(12, 'laurawilson', 'H3ll0W0rld$', 'Employee', 'Laura', 'Wilson', 'laura.wilson@example.com', '234-567-8901', '987 Oak St\nSan Francisco, CA 94102', '2022-07-30', 'HR', 'Recruiter', '1993-04-12', 'Paul Wilson', '345-678-9012', 'Parent', 8, 0),
+(13, 'davidlee', 'R3d@ppl3#8', 'Employee', 'David', 'Lee', 'david.lee@example.com', '678-901-2345', '321 Birch St\nBoston, MA 02118', '2021-04-11', 'Operations', 'Coordinator', '1991-11-05', 'Linda Lee', '789-012-3456', 'Spouse', 10, 0),
+(14, 'userone@mail.com', '$2y$10$TyVjOXMSbiBXTqzup54Yd.GkQG/XtVg82X6xOnSo.0BHRMhjqjJq6', 'project manager', 'user', 'one', 'userone@mail.com', '1111111111', 'address 5', '2024-07-01', 'Front end', 'project manager', '2024-07-02', NULL, NULL, NULL, NULL, 0),
+(15, 'empone@mail.com', '$2y$10$jg9JANbRXR8yypI2nakXGeMnFkUCRsTmCpZcuPJO4IuAGlmsKCE/2', 'employee', 'emp', 'one', 'empone@mail.com', '1111111111', 'address 2', '2024-07-01', 'back end', 'developer', '2024-07-01', NULL, NULL, NULL, 4, 20),
+(16, 'ob@mail.com', '$2y$10$WEiofwoUwxCzyc6KuX4r6uaXK/g00iaCuTL0J8Vy9UTC58H5.KGMG', 'HR', 'Bob', 'Brown', 'ob@mail.com', '1111111111', 'address123', '2020-08-01', 'HR', 'HR', '1996-01-01', NULL, NULL, NULL, NULL, 0),
+(17, 'anaij@mail.com', '$2y$10$xR8muB7UXejd.9X0jPU6neEKoifzeOrv8BYno8tKG70Js9SAN7Mti', 'employee', 'anai', 'joshy', 'anaij@mail.com', '1234567890', 'address231', '2024-07-01', 'development', 'employee', '2000-02-08', NULL, NULL, NULL, NULL, 0);
 
 --
 -- Indexes for dumped tables
@@ -323,7 +340,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `complaints`
@@ -335,7 +352,7 @@ ALTER TABLE `complaints`
 -- AUTO_INCREMENT for table `leaverequests`
 --
 ALTER TABLE `leaverequests`
-  MODIFY `leave_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `leave_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `projects`
@@ -347,7 +364,7 @@ ALTER TABLE `projects`
 -- AUTO_INCREMENT for table `salaries`
 --
 ALTER TABLE `salaries`
-  MODIFY `salary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `salary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `tasks`
